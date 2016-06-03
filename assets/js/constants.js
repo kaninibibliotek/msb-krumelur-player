3338f473f635aec387c01e298d7ec3cd06342f04
erik <erik@unswornindustries.com>
2016-06-03 15:33:39 +0200

    Color z-constants

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -12,4 +12,18 @@ var constants = {
   URL_RANDOM: '/api/krumelur/random/',
 
   DEFAULT_BEHAVIOR: 'calm',
+
+  MASK_COLORS: {
+    '100': 0xff00ff,
+    '90': 0x8000ff,
+    '80': 0x0000ff,
+    '70': 0x0080ff,
+    '60': 0x00ffff,
+    '50': 0x00ff80,
+    '40': 0x00ff00,
+    '30': 0x80ff00,
+    '20': 0xffff00,
+    '10': 0xff8000,
+    '0': 0xff0000,
+  }
 };
