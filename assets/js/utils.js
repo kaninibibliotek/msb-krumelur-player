52aa14ce2a5ed8e894d3742dddafb4b23bf34412
erik <erik@unswornindustries.com>
2016-08-29 14:54:04 +0200

    Forgot to add utils

---
+++ assets/js/utils.js
@@ -0,0 +1,49 @@
+var utils = (function() {
+  var dev = window.location.search.startsWith('?dev');
+
+  return {
+    isDev: function() {
+      return dev;
+    },
+
+    getQueryValue: function(field) {
+      var query = window.location.search.substring(4);
+      var index = query.indexOf(field);
+
+      if (index === -1) {
+        return null;
+      }
+
+      var next = query.substring(index).indexOf('&');
+
+      if (next === -1) {
+        next = query.length;
+      }
+
+      return query.substring(index + field.length + 1, next + 1);
+    },
+
+    // '001' ... '100' --> 'behaviorName'
+    behaviorKeyToName: function(strKey) {
+      const key = parseInt(strKey);
+      
+      if (key < 20) {
+        return 'newton2';
+      } else if (key < 40) {
+        return 'newton3';
+      } else if (key < 60) {
+        return 'newton4';
+      } else if (key < 80) {
+        return 'newton5';
+      } else if (key < 100) {
+        return 'scaletestet';
+      } else {
+        return DEFAULT_BEHAVIOR; 
+      }
+    },
+
+    getRandomInt: function(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+  };
+})();
